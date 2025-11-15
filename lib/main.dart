73a58f13ca5d444b1524f92aec74362d5d61ee36
Tasso Hibung <hibungnaga@gmail.com>
2025-11-16 03:32:34 +0530

    basic theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,40 @@
 import 'package:flutter/material.dart';
-import 'routes.dart';
+
+//------THEME------
+final lightColorScheme = ColorScheme.fromSeed(seedColor: Colors.purpleAccent);
+final darkColorScheme = ColorScheme.fromSeed(brightness: Brightness.dark,seedColor: const Color.fromARGB(255, 31, 7, 79));
+
+//Light Theme
+final lightTheme = ThemeData().copyWith(
+  colorScheme: lightColorScheme,
+  appBarTheme: AppBarTheme().copyWith(
+    backgroundColor: lightColorScheme.onPrimaryContainer,
+    foregroundColor: Colors.white,
+  ),
+  textTheme: TextTheme().copyWith(
+    titleLarge: TextStyle(color: Colors.white),
+    titleMedium: TextStyle(color: Colors.white),
+    titleSmall: TextStyle(color: Colors.white),
+  )
+);
+
+//Dark THEME
+final darkTheme = ThemeData().copyWith(
+  colorScheme: darkColorScheme,
+);
 
 void main() {
-  runApp(const FinanceApp());
+  runApp(const MyApp());
 }
 
-class FinanceApp extends StatelessWidget {
-  const FinanceApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Finance Management System',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.indigo,
-      ),
-      initialRoute: Routes.splash,
-      onGenerateRoute: Routes.generate,
-    );
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      );
   }
 }
