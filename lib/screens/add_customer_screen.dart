6cb8df926470315f4c8b9cd80bbfc71b834c09d5
Tasso Hibung <hibungnaga@gmail.com>
2025-11-24 04:00:17 +0530

    toggle Credit/Debit slider in AddCustomerScreen

--- lib/screens/add_customer_screen.dart
+++ lib/screens/add_customer_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:khata_king/db/db_helper.dart';
 import 'package:khata_king/models/customers.dart';
 import 'package:khata_king/providers/customer_providers.dart';
 import 'package:khata_king/providers/navigation_provider.dart';
+import 'package:khata_king/widgets/toggle_credit_debit.dart';
 
 class AddCustomerScreen extends ConsumerStatefulWidget {
   const AddCustomerScreen({super.key});
@@ -16,7 +18,11 @@ class AddCustomerScreen extends ConsumerStatefulWidget {
 
 class _AddCustomerState extends ConsumerState<AddCustomerScreen> {
   //getting ready the data for creating new customer object
-  String? _name, _phone;
+  String? _name, _phone, _type;
+  double? _balance;
+
+  //Controllers
+  final _balanceController = TextEditingController(text: "0");
 
   //Form Key
   final _formKey = GlobalKey<FormState>();
@@ -39,23 +45,24 @@ class _AddCustomerState extends ConsumerState<AddCustomerScreen> {
       name: _name!,
       phone: _phone!,
       created_date: createdDate,
-      balance: 0,
+      balance:
+          _balance ??
+          0, //return the balance || if balance is null then return 0
     );
 
     //Insert into DB/DB table
     DbHelper.instance.addCustomer(customer);
 
     ScaffoldMessenger.of(context).clearSnackBars();
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text("$_name added into Database"))
-    );
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text("$_name added into Database")));
 
     //Refresh customerListProvider
     ref.invalidate(customerListProvider);
 
-    //Pop to Dashboard 
-    ref.read(navigationProvider.notifier).state=0;
-    
+    //Pop to Dashboard
+    ref.read(navigationProvider.notifier).state = 0;
   }
 
   @override
@@ -64,81 +71,164 @@ class _AddCustomerState extends ConsumerState<AddCustomerScreen> {
       body: Container(
         padding: EdgeInsets.all(20),
 
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              //Customer Name
-              TextFormField(
-                onSaved: (newValue) {
-                  _name = newValue;
-                },
-                validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      value.trim().length > 50) {
-                    return "Please enter valid customer name";
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  hintText: "Customer Name",
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Theme.of(
-                        context,
-                      ).colorScheme.primary.withAlpha(200),
-                      width: 1,
+        child: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                //Customer Name
+                TextFormField(
+                  onSaved: (newValue) {
+                    _name = newValue;
+                  },
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length > 50) {
+                      return "Please enter valid customer name";
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Customer Name",
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(
+                          context,
+                        ).colorScheme.primary.withAlpha(200),
+                        width: 1,
+                      ),
                     ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Theme.of(context).colorScheme.primary,
-                      width: 2,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: 10),
-
-              //Phone number
-              TextFormField(
-                onSaved: (newValue) {
-                  _phone = newValue;
-                },
-                validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      value.trim().length < 10 ||
-                      value.trim().length > 10) {
-                    return "Please enter 10 digits valid phone number";
-                  }
-                  return null;
-                },
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  hintText: "Phone Number",
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Theme.of(
-                        context,
-                      ).colorScheme.primary.withAlpha(200),
-                      width: 1,
-                    ),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Theme.of(context).colorScheme.primary,
-                      width: 2,
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.primary,
+                        width: 2,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 10),
+                SizedBox(height: 10),
 
-              //Save Button
-              OutlinedButton(onPressed: _onSave, child: Text("Save")),
-            ],
+                //Phone number
+                TextFormField(
+                  onSaved: (newValue) {
+                    _phone = newValue;
+                  },
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length < 10 ||
+                        value.trim().length > 10) {
+                      return "Please enter 10 digits valid phone number";
+                    }
+                    return null;
+                  },
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    hintText: "Phone Number",
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(
+                          context,
+                        ).colorScheme.primary.withAlpha(200),
+                        width: 1,
+                      ),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.primary,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+
+                Row(
+                  children: [
+                    //Amount
+                    SizedBox(
+                      width: 150,
+                      child: TextFormField(
+                        controller: _balanceController,
+                        onChanged: (value){
+                          setState(() {
+                            //+++++++++++++++++++++++++++++++
+                          });
+                        },
+                        // initialValue: "0",
+                        onSaved: (newValue) {
+                          //if new value is null return null, if tryParse return null, then return 0 (double)
+                          _balance = double.tryParse(newValue ?? '') ?? 0;
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Enter Amount";
+                          }
+                          if (double.tryParse(value) == null) {
+                            return "Only numbers allowed";
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          labelText: "Amount [OPTIONAL]",
+                          prefixText: "Rs. ",
+                          // hintText: "0",
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Theme.of(context).colorScheme.primary,
+                              width: 1,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              width: 2,
+                              color: Theme.of(context).colorScheme.primary,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+
+                    //Amount Type
+                    SizedBox(
+                      width: 200,
+                      child: ToggleCreditDebit(
+                        onChange: (value) {
+                          _type = value;
+                        },
+                        isAmountNotNull: (double.tryParse(_balanceController.text) ?? 0) != 0,
+                      ),
+                    ),
+                  ],
+                ),
+
+                SizedBox(height: 10),
+
+                //Save Button
+                SizedBox(
+                  width: double.infinity,
+
+                  child: OutlinedButton(
+                    onPressed: _onSave,
+                    style: OutlinedButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text(
+                      "Save",
+                      style: Theme.of(
+                        context,
+                      ).textTheme.titleMedium!.copyWith(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/widgets/toggle_credit_debit.dart
@@ -0,0 +1,203 @@
+import 'package:flutter/material.dart';
+
+class ToggleCreditDebit extends StatefulWidget {
+  const ToggleCreditDebit({
+    super.key,
+    required this.onChange,
+    required this.isAmountNotNull,
+  });
+
+  //onChange Function
+  final Function(String) onChange;
+
+  //Amount entered checker
+  final bool isAmountNotNull;
+
+  @override
+  State<ToggleCreditDebit> createState() {
+    return _ToggleCreditDebitState();
+  }
+}
+
+class _ToggleCreditDebitState extends State<ToggleCreditDebit> {
+  //by default type = Credit
+  bool isCredit = true;
+
+  //Initial  Widget
+    final activeWidget = Stack(
+      children: [
+        // Animation Layer
+        //Dynamic slider size
+        LayoutBuilder(
+          builder: (ctx, constraints) {
+            return AnimatedAlign(
+              //Meta data
+              alignment: Alignment.centerLeft,
+              duration: const Duration(milliseconds: 200),
+
+              //Slider UI
+              child: Container(
+                width: constraints.maxWidth / 2, //Dynamic UI Slider
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 10,
+                      spreadRadius: 1,
+                      offset: Offset(3, 3),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
+        ),
+
+        // Credit and Debit Text Layer
+        Row(
+          children: [
+            // CREDIT (You Gave)
+            Expanded(
+              child: GestureDetector(
+                onTap: () {},
+                child: Center(
+                  child: Text(
+                    "You Gave",
+
+                  ),
+                ),
+              ),
+            ),
+
+            // DEBIT (You Got)
+            Expanded(
+              child: GestureDetector(
+                onTap: () {},
+                child: Center(
+                  child: Text(
+                    "You Got",
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+
+  @override
+  Widget build(BuildContext context) {
+
+    //Background
+    return Opacity(
+      opacity: widget.isAmountNotNull ? 1 : 0.4,
+      child: Container(
+        height: 58,
+        width: double.infinity,
+        padding: const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: Colors.grey.shade300,
+          borderRadius: BorderRadius.circular(10),
+        ),
+
+        child: widget.isAmountNotNull? Stack(
+          children: [
+            // Animation Layer
+            //Dynamic slider size
+            LayoutBuilder(
+              builder: (ctx, constraints) {
+                return AnimatedAlign(
+                  //Meta data
+                  alignment: isCredit
+                      ? Alignment.centerLeft
+                      : Alignment.centerRight,
+                  duration: const Duration(milliseconds: 200),
+
+                  //Slider UI
+                  child: Container(
+                    width: constraints.maxWidth / 2, //Dynamic UI Slider
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 10,
+                          spreadRadius: 1,
+                          offset: Offset(3, 3),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+
+            // Credit and Debit Text Layer
+            Row(
+              children: [
+                // CREDIT (You Gave)
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isCredit = true;
+                      });
+                      widget.onChange("credit"); //Notify Parent -> in AddCustomerScreen
+                    },
+                    child: Center(
+                      child: Text(
+                        "You Gave",
+                        style: isCredit
+                            ? Theme.of(context).textTheme.titleMedium!.copyWith(
+                                color: Colors.red,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              )
+                            : Theme.of(context).textTheme.titleMedium!.copyWith(
+                                color: Colors.red,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                // DEBIT (You Got)
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        isCredit = false;
+                      });
+                      widget.onChange("debit"); //Notify Parent -> in AddScreenScreen
+                    },
+                    child: Center(
+                      child: Text(
+                        "You Got",
+                        style: isCredit
+                            ? Theme.of(context).textTheme.titleMedium!.copyWith(
+                                color: Colors.green,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              )
+                            : Theme.of(context).textTheme.titleMedium!.copyWith(
+                                color: Colors.green,
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                              ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ): activeWidget,
+      ),
+    );
+  }
+}
